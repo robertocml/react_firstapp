90829808c7c8576f033d010566e59c3c7d937bfb
robertocml <roberto.perezb@outlook.com>
2020-01-06 22:06:40 -0600

    initial

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
 
   const APP_ID = '37867009';
-  const APP_KEY = 'cb41b4ace52fa6c20df46bd16bbb13ee';
-  const exampleReq = "https://api.edamam.com/search?q=chicken&app_id=${37867009}&app_key=${cb41b4ace52fa6c20df46bd16bbb13ee}"
+  const APP_KEY = 'cb41b4ace52fa6c20df46bd16bbb13ee';  
+  const [counter, setCounter] = useState(0);  
 
+  useEffect( () =>{
+    console.log("Effect has been run");
+    getRecipes();
+
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+    `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      );
+    const data = await response.json();
+    console.log(data);
+  } 
 
   return(
     <div className="App">
       <h1>Hello React</h1>
-
+      <form className="search-from">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">
+          search
+        </button>
+      </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   )
 }
